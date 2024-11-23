93b8d49da9e041ee5d0206152eb212ca0e39b6fc
Jenia Dysin <genia90@gmail.com>
2024-11-23 22:35:55 +0000

    update nvim-cmp config

--- lua/jenia90/plugins/nvim-cmp.lua
+++ lua/jenia90/plugins/nvim-cmp.lua
@@ -41,7 +41,8 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
         ['<C-e>'] = cmp.mapping.abort(), -- close completion window
-        ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<tab>'] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm { select = false },
       },
       -- sources for autocompletion
       sources = cmp.config.sources {
